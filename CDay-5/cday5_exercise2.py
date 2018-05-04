e6f9d5412d30ef81dcdbb946f9cb483b315d09a3
xljnc <jsj0804wt@126.com>
2018-05-04 23:05:03 +0800

    [A] CDay-5 Exercise2 by Xljnc 2018-May-04

---
+++ CDay-5/cday5_exercise2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# @Time      :2018/5/4 22:42
+# @Author    :WuTian
+# @Contact   :jsj0804wt@126.com
+# @Desc
+import math
+
+print ("12 * 34 + 78 - 132 / 6 = %d" % (12 * 34 + 78 - 132 / 6))
+print ("(12 * (34 + 78) - 132) / 6 = %d" % ((12 * (34 + 78) - 132) / 6))
+print ("mod(145,23) = %d" % (145 % 23))
+print ("145%%23 = %d" % (math.fmod(145, 23)))
+print ("sin (0.5) = %f" % math.sin(0.5))
+print ("cos (0.5) = %f" % math.cos(0.5))
